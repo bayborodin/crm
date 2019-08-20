ba346ec390211471778d74fc178d1eb7377262e2
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-08-20 20:50:16 +1000

    edit project settings

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '185.185.70.176']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
