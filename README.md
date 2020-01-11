c832dee85f578c0ebee82cc5f36d90d1c324f901
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-01-11 20:44:03 +1000

    add test coverage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Django CRM
 CRM built with Django 2.0
+
+## Тесты
+```bash
+coverage erase  # Remove any coverage data from previous runs
+coverage run manage.py test  # Run the full test suite
+coverage run manage.py test offerings -v 2
+coverage report --include=./*py
+coverage html --include=./*py
---
+++ crm/accounts/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from accounts.models import AccountType
+
+
+class AccountTypeTest(TestCase):
+
+    def create_account_type(self, name="test account type", tsid="000-00-000-000"):
+        return AccountType.objects.create(name=name, tsid=tsid)
+
+    def test_account_type_creation(self):
+        acc_type = self.create_account_type()
+        self.assertTrue(isinstance(acc_type, AccountType))
+        self.assertEqual(acc_type.__str__(), acc_type.name)
