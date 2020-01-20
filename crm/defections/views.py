3cfb66b5b4b9e1bb9b7d9c40f9b5c50b7b9b98df
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-01-20 23:04:14 +1000

    convert account_extid to uppercase

--- crm/defections/views.py
+++ crm/defections/views.py
@@ -4,6 +4,6 @@ from .models import Defection
 
 
 def index(request, account_extid):
-    defections = Defection.objects.filter(account__extid=account_extid)
+    defections = Defection.objects.filter(account__extid=account_extid.upper())
     context = {'defections': defections}
     return render(request, 'defections/index.html', context)
