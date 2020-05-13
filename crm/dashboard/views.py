d8d0e039abf50b9d5be4a8de8036762be38a7aec
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-05-13 15:33:42 +1000

    refactoring

--- crm/dashboard/views.py
+++ crm/dashboard/views.py
@@ -17,18 +17,16 @@ def index(request):
     today = datetime.today()
     previous_week_day = today + timedelta(days=-7)
 
-    order_metric = Metric.objects.get(name='Заказ')
+    order_metric = Metric.objects.get(name="Заказ")
 
     order_statistics_current = WeekResult.objects.filter(
-        metric=order_metric,
-        year=today.year,
-        week=today.isocalendar()[1]
+        metric=order_metric, year=today.year, week=today.isocalendar()[1]
     )
 
     order_statistics_previous = WeekResult.objects.filter(
         metric=order_metric,
         year=previous_week_day.year,
-        week=previous_week_day.isocalendar()[1]
+        week=previous_week_day.isocalendar()[1],
     )
 
     if order_statistics_current.exists():
@@ -40,20 +38,24 @@ def index(request):
         orders_sum_prev_week = order_statistics_previous[0].val
 
     if orders_cnt_prev_week == 0:
-        orders_cnt_percent = '__'
+        orders_cnt_percent = "__"
     else:
-        orders_cnt_percent = round((orders_cnt_cur_week - orders_cnt_prev_week) / orders_cnt_prev_week * 100)
+        orders_cnt_percent = round(
+            (orders_cnt_cur_week - orders_cnt_prev_week) / orders_cnt_prev_week * 100
+        )
 
     if orders_sum_prev_week == 0:
-        orders_sum_percent = '__'
+        orders_sum_percent = "__"
     else:
-        orders_sum_percent = round((orders_sum_cur_week - orders_sum_prev_week) / orders_sum_prev_week * 100)
+        orders_sum_percent = round(
+            (orders_sum_cur_week - orders_sum_prev_week) / orders_sum_prev_week * 100
+        )
 
     context = {
-        'section': 'dashboard',
-        'orders_cnt': orders_cnt_cur_week,
-        'orders_cnt_percent': orders_cnt_percent,
-        'orders_sum': orders_sum_cur_week,
-        'orders_sum_percent': orders_sum_percent,
+        "section": "dashboard",
+        "orders_cnt": orders_cnt_cur_week,
+        "orders_cnt_percent": orders_cnt_percent,
+        "orders_sum": orders_sum_cur_week,
+        "orders_sum_percent": orders_sum_percent,
     }
-    return render(request, 'dashboard/index.html', context)
+    return render(request, "dashboard/index.html", context)
