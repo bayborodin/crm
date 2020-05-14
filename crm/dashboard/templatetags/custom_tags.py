c45e3f04f74e19626b18f6ecdbddadc0f7e5c8d5
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-05-14 10:19:33 +1000

    Implement  space s between digits

--- crm/dashboard/templatetags/custom_tags.py
+++ crm/dashboard/templatetags/custom_tags.py
@@ -5,4 +5,5 @@ register = template.Library()
 
 @register.filter(name="thousands")
 def thousands_format(num):
-    return round(num / 1000)
+    num = round(num / 1000)
+    return f"{num:,}".replace(",", " ")
